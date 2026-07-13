21b35ef5eceddcf928f456b448532fd914d2e758
Igor Chaves Donega <236210133+ichavesbr@users.noreply.github.com>
2026-07-14 02:59:48 +0900

    add GET user endpoint with name filtering

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  // GET /user
+  @Get()
+  getUsers(@Query('name') name: string) {
+    const users = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Igor' },
+    ];
+
+    if (name) {
+      return users.filter((user) =>
+        // verifica se dentro do array em name,
+        // tem algo que foi digitado pelo usuario que eh recebido pela prop name
+        user.name.toLowerCase().includes(name.toLowerCase()),
+      );
+    }
+    return users;
+  }
+}
