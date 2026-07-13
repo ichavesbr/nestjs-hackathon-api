73de17158410a615070d336eeb118c271d4942ab
Igor Chaves Donega <236210133+ichavesbr@users.noreply.github.com>
2026-07-14 08:53:34 +0900

    create new HTTP methods and fix comments

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,38 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
-@Controller('user')
+@Controller('user') // rota /user
 export class UserController {
-  // GET /user
-  @Get()
+  @Get() // cria método HTTP GET
+  // cria método para buscar query name na url. --> /user?name=...
   getUsers(@Query('name') name: string) {
     const users = [
       { id: 1, name: 'John' },
       { id: 2, name: 'Igor' },
     ];
 
+    // verifica se tem o parâmetro "name" na url. --> /user?name=...
     if (name) {
       return users.filter((user) =>
-        // verifica se dentro do array em name,
-        // tem algo que foi digitado pelo usuario que eh recebido pela prop name
+        // verifica se dentro do array "users" tem algum name igual ao do parâmetro da url
         user.name.toLowerCase().includes(name.toLowerCase()),
       );
     }
     return users;
   }
+
+  @Get(':id') // cria método HTTP GET com id dinâmico
+  // cria método para buscar parâmetro id na url. --> /user/123...
+  getUserById(@Param('id') id: string) {
+    return { id, name: 'Igor' };
+  }
+
+  @Post() // cria método HTTP POST
+  // cria método para criar user novo. --> /user/
+  createUser(@Body() body: any) {
+    return { message: 'User created successfully' };
+  }
 }
+
+// DTO - data transfer object
+// classe com os tipos do TS para o body do POST
+// não usa type nem interface, e sim class
