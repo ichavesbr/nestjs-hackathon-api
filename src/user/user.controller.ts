f069c060d61375c66b940f1e1953b7e0e8858501
Igor Chaves Donega <236210133+ichavesbr@users.noreply.github.com>
2026-07-14 09:11:15 +0900

    add more HTTP methods on user model

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user') // rota /user
 export class UserController {
-  @Get() // cria método HTTP GET
+  @Get() // cria método HTTP GET --> /user/igor
   // cria método para buscar query name na url. --> /user?name=...
   getUsers(@Query('name') name: string) {
     const users = [
@@ -20,19 +22,24 @@ export class UserController {
     return users;
   }
 
-  @Get(':id') // cria método HTTP GET com id dinâmico
+  @Get(':id') // cria método HTTP GET com id dinâmico --> /user/:id
   // cria método para buscar parâmetro id na url. --> /user/123...
   getUserById(@Param('id') id: string) {
     return { id, name: 'Igor' };
   }
 
-  @Post() // cria método HTTP POST
-  // cria método para criar user novo. --> /user/
-  createUser(@Body() body: any) {
-    return { message: 'User created successfully' };
+  @Post() // cria método HTTP POST --> /user
+  // cria método para criar user novo. createUserDto é o body do POST.
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return { data: createUserDto, message: 'User created successfully' };
+  }
+
+  @Put() // cria método HTTP PUT --> /user/:id
+  // cria método para atualizar user existente. updateUserDto é o body do POST.
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return {
+      data: { id, ...updateUserDto },
+      message: 'User created successfully',
+    };
   }
 }
-
-// DTO - data transfer object
-// classe com os tipos do TS para o body do POST
-// não usa type nem interface, e sim class
