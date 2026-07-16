19e6896506be79dda63ac80d68b3f2e4afbf646c
Igor Chaves Donega <236210133+ichavesbr@users.noreply.github.com>
2026-07-16 16:18:30 +0900

    fix createUser method and CreateUserDto class

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,13 +2,13 @@
 // classes com os tipos do TS para o body do POST
 // não usa type nem interface, e sim class
 
-import { isEmail, isString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @isString()
+  @IsString()
   @MinLength(3)
   name!: string;
 
-  @isEmail()
+  @IsEmail()
   email!: string;
 }
