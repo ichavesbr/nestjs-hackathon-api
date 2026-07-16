c66145a9961d556aaa73e5bd1c9558aecb98b17a
Igor Chaves Donega <236210133+ichavesbr@users.noreply.github.com>
2026-07-16 14:53:02 +0900

    create validator pipe, fix tsconfig.json

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './utils/transform.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new TransformInterceptor()); // tipo um middleware
+
+  app.useGlobalPipes(new ValidationPipe()); // executa antes do controller
+  app.useGlobalInterceptors(new TransformInterceptor()); // executa após controller (tipo um middleware)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,6 +2,13 @@
 // classes com os tipos do TS para o body do POST
 // não usa type nem interface, e sim class
 
+import { isEmail, isString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
-  name: string;
+  @isString()
+  @MinLength(3)
+  name!: string;
+
+  @isEmail()
+  email!: string;
 }
